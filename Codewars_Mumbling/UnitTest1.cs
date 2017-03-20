d69b4350c6b845d3ab2ae957c0487360d45d148f
hatelove <jay.hatelove@gmail.com>
2017-03-20 14:32:04 +0800

    intialized and add the first failed test: s is "b", should return "B"

---
+++ Codewars_Mumbling/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_Mumbling
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void s_is_b_Accum_should_be_B()
+        {
+            var s = "b";
+            Assert.AreEqual("B", Accumul.Accum(s));
+        }
+    }
+
+    public class Accumul
+    {
+        public static String Accum(string s)
+        {
+            // your code
+            throw new NotImplementedException();
+        }
+    }
+}
